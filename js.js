0a7e318851e1f8c88623dc419170fdcc88baab32
ShidoJP <familiadojeronimo@gmail.com>
2023-03-18 13:24:28 -0300

    parte_2_string

--- js.js
+++ js.js
@@ -1,5 +1,20 @@
-let a, b, c;
-a = 10;
-b = 5;
+/*
+Podiamos declarar usando var, mais esse tipo de declaração se tornou
+deprecada, pois ela tem alguns bugs, em alguns metodos utilizados se 
+num codigo extenso, ela pode dificultar a encontrar por exemplo um 
+erro no código, pois ela não da info de erro ou algo parecido.
+*/
+
+//Declaração de numero
+let a = 10, b = 5, c;
 c = a+b;
-console.log(c);
+console.log(c);     //comando que faz a saida de informações pelo terminal
+
+//Declaração de string
+let nome = "Jeronimo", sobreNome = "Noleto";
+/*Percebece que usamos a mesma declaração let, o que muda que para ser uma
+string colocamos ela entre "aspas".
+*/
+
+nome += sobreNome;      //Concatenação
+console.log(nome);      //Saida
