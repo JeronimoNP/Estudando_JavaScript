624aac4b942654f7a65c0e5a04ffdb365e2221b7
ShidoJP <familiadojeronimo@gmail.com>
2023-03-21 22:54:36 -0300

    codigo com novos recursos

--- js.js
+++ js.js
@@ -12,9 +12,21 @@ console.log(c);     //comando que faz a saida de informações pelo terminal
 
 //Declaração de string
 let nome = "Jeronimo", sobreNome = "Noleto";
-/*Percebece que usamos a mesma declaração let, o que muda que para ser uma
+/* Percebece que usamos a mesma declaração let, o que muda que para ser uma
 string colocamos ela entre "aspas".
 */
-
 nome += sobreNome;      //Concatenação
 console.log(nome);      //Saida
+
+// Podemos declara uma variavel para ser imutavel usando const
+const numb3 = 10;
+/*numb3 = 10 + 2;
+Se esse codigo for executado ele vai dar erro pois como declaramos ela como
+uma constante, ela não pode ser alterada*/
+
+/* Agora para descobrirmos se uma numero é impar ou par podemos utilizar o
+operador de resto, se der 0 significa que é par, se der 1 significa que é 
+impar */
+//laço para descobrir se a condição é verdadeira
+let number1 = 21, resto;
+resto = number1 % 2;
